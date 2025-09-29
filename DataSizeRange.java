b3295174c3003514c2a25de764f6d935e1d41378
Naveen Kumar <naveen330@example.com>
2025-09-29 23:14:19 +0530

    DataSizaRange

---
+++ DataSizeRange.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class DataSizeRange {
+    public static void main(String args[]) {
+        System.out.println("Float Min :" + Float.MIN_VALUE);
+        System.out.println("Float Max :" + Float.MAX_VALUE);
+        System.out.println("Float Bytes" + Float.SIZE);
+        System.out.println("Int Min :" + Integer.MIN_VALUE);
+        System.out.println("Int Max :" + Integer.MAX_VALUE);
+        System.out.println("Int Byte :" + Integer.SIZE);
+        System.out.println("Char Min :" + Character.MIN_VALUE);
+        System.out.println("Char Max :" + (int) Character.MAX_VALUE);
+        System.out.println("Char Bytes :" + Character.SIZE);
+
+    }
+
+}
