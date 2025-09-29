4283f863dd814ee43b04649fe973025f7decabcd
Naveen Kumar <naveen330@example.com>
2025-09-29 21:46:20 +0530

    KeybRead

---
+++ KeybRead.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class KeybRead {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        String name;
+        System.out.println("May i know your name");
+        System.out.println("h");
+        System.out.println("k");
+        name = s.nextLine();
+        System.out.println("Welcome mr" + name);
+        s.close();
+    }
+
+}
