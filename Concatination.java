998e830cac1dd48df6c8f3b15a57179e1dee1937
Naveen Kumar <naveen330@example.com>
2026-02-07 21:46:48 +0530

    Learning Concatination

---
+++ Concatination.java
@@ -0,0 +1,8 @@
+
+public class Concatination {
+    public static void main(String args[]) {
+        int x = 4, y = 7;
+        System.out.println("sum Of " + 10 + " and " + 20 + " is " + 30);
+    }
+
+}
