dab4ef186ce898677cdfb32f4712299634d93f1b
Naveen Kumar <naveen330@example.com>
2026-02-09 19:16:34 +0530

    Swapping

---
+++ SwapTwo.java
@@ -0,0 +1,11 @@
+public class SwapTwo {
+    public static void main(String args[]) {
+        int a = 22, b = 33;
+        int temp = 0;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("Swap a and b " + a + " " + b);
+    }
+
+}
