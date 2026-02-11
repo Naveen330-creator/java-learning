fff73e866afded79ff2938525cbc55c65cdeb716
Naveen Kumar <naveen330@example.com>
2026-02-11 23:58:21 +0530

    StringPractice

---
+++ StringPractice3.java
@@ -0,0 +1,11 @@
+public class StringPractice3 {
+    public static void main(String args[]) {
+        String str = "Java";
+        int l = str.length();
+        String str1 = str.toLowerCase();
+        System.out.println(l);
+        System.out.println(str1);
+
+    }
+
+}
