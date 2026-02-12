485d1baf5786f929500d2e9f55fabdae0c1c43d6
Naveen Kumar <naveen330@example.com>
2026-02-12 11:09:03 +0530

    StringPractice

---
+++ PracticeString4.java
@@ -0,0 +1,8 @@
+public class PracticeString4 {
+    public static void main(String args[]) {
+        String str = "Welcome";
+        String str1 = str.substring(3, 6);
+        System.out.println(str1);
+    }
+
+}
