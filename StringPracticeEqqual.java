21cc3ba06da70521e727c323fb61d5047bc00236
Naveen Kumar <naveen330@example.com>
2026-02-12 13:36:11 +0530

    learningstrnig

---
+++ StringPracticeEqqual.java
@@ -0,0 +1,9 @@
+public class StringPracticeEqqual {
+    public static void main(String args[]) {
+        String str = "NaveenDubey";
+        String str1 = "Naveendubey";
+        System.out.println(str.equals(str1));
+        System.out.println(str.equalsIgnoreCase(str1));
+    }
+
+}
