5a130d9bd541d834530063654f01db7ef746b978
Naveen Kumar <naveen330@example.com>
2026-02-12 11:49:03 +0530

    EndswithStartswith

---
+++ EndswithStartswith.java
@@ -0,0 +1,13 @@
+public class EndswithStartswith {
+    public static void main(String args[]) {
+        String str = "www.abcd.org";
+        if (str.endsWith(".org")) {
+
+            System.out.println("It is world wide web");
+        } else {
+            System.out.println("Not world wide web");
+        }
+
+    }
+
+}
