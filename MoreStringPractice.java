a74dfb7bca5e54afd5895d3af1f63ca49cbc94a3
Naveen Kumar <naveen330@example.com>
2026-02-12 13:18:30 +0530

    StringPractice

---
+++ MoreStringPractice.java
@@ -0,0 +1,9 @@
+public class MoreStringPractice {
+    public static void main(String args[]) {
+        String str = "Mr. Naveen Dubey";
+        System.out.println(str.charAt(4));
+        System.out.println(str.indexOf('N'));
+
+    }
+
+}
