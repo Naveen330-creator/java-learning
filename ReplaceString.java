26775a19a8ab65a030bc48fcf4fc1d71248021d1
Naveen Kumar <naveen330@example.com>
2026-02-12 11:19:12 +0530

    ReplacingString

---
+++ ReplaceString.java
@@ -0,0 +1,10 @@
+public class ReplaceString {
+    public static void main(String args[]) {
+        String str = "WelcomeHere";
+        String str1 = str.replace('W', 'e');
+        // String str1 = str.replace('e', 'c');
+        System.out.println(str1);
+
+    }
+
+}
