ca864a4b7c9d5f46212e1bf25b6c19babd3657db
Naveen Kumar <naveen330@example.com>
2026-02-13 15:08:01 +0530

    String Finding domain and Username from gmail

---
+++ StringQuestion.java
@@ -0,0 +1,12 @@
+public class StringQuestion {
+    public static void main(String args[]) {
+        String str = "Naveendubey164@gmail.com";
+        int i = str.indexOf("@");
+        String uname = str.substring(0, i);
+        String domain = str.substring(i + 1, str.length());
+        System.out.println("Username " + uname);
+        System.out.println("Domain " + domain);
+
+    }
+
+}
