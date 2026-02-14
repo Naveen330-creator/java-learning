81e47bd3a8496e8bef9c85b7f0e1c5bd71fdd945
Naveen Kumar <naveen330@example.com>
2026-02-14 22:50:09 +0530

    LoopPractice

---
+++ PracticeLoop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PracticeLoop {
+    public static void main(String args[]) {
+        System.out.println("Enter n ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
