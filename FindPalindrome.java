404c83d718398291bf103bdd942193e04721787f
Naveen Kumar <naveen330@example.com>
2026-02-20 00:10:52 +0530

    Palindrome

---
+++ FindPalindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FindPalindrome {
+    public static void main(String args[]) {
+        System.out.println("Enter a number ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n;
+        int r, rev = 0;
+        while (n > 0) {
+            r = n % 10;
+            rev = (rev * 10) + r;
+            n = n / 10;
+
+        }
+        if (rev == temp) {
+            System.out.println("The number is palindrome ");
+        } else {
+            System.out.println("The number is not palindrome ");
+        }
+    }
+
+}
