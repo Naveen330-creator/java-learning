8071e68313f4c23c759ca4efa36592bec2f8a1ae
Naveen Kumar <naveen330@example.com>
2026-02-24 00:11:06 +0530

    Pattern3

---
+++ Pattern3.java
@@ -0,0 +1,14 @@
+public class Pattern3 {
+    public static void main(String args[]) {
+        System.out.println("Print Pattern ");
+        int count = 0;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                count++;
+                System.out.format("%2d ", count);
+            }
+            System.out.println("");
+        }
+    }
+
+}
