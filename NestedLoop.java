fd0dbf0a5c7c6c9865533ae8fb78e82291adc87a
Naveen Kumar <naveen330@example.com>
2026-02-23 23:24:17 +0530

    Nestedloop

---
+++ NestedLoop.java
@@ -0,0 +1,12 @@
+public class NestedLoop {
+    public static void main(String args[]) {
+        System.out.println("print pattern ");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("(" + i + "," + j + ")");
+            }
+            System.out.println("");
+        }
+    }
+
+}
