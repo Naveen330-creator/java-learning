02f4e5a31e479b4497ed33ebe115f856e8266988
Naveen Kumar <naveen330@example.com>
2026-03-01 00:49:50 +0530

    Array1

---
+++ array.java
@@ -0,0 +1,9 @@
+public class array {
+    public static void main(String args[]) {
+        int A[] = { 2, 4, 6, 7 };
+        for (int i = 0; i < A.length; i++) {
+            System.out.print(A[i] + ",");
+        }
+    }
+
+}
