a8d1c6d57fee1cb3ff6b48dd00b6d89b1e643a81
Naveen Kumar <naveen330@example.com>
2026-03-02 00:00:17 +0530

    Array1

---
+++ Array1.java
@@ -0,0 +1,9 @@
+public class Array1 {
+    public static void main(String args[]) {
+        int A[] = { 1, 2, 3, 4, 5 };
+        for (int i = A.length - 1; i >= 0; i--) {
+            System.out.println(A[i]);
+        }
+    }
+
+}
