7519179ae0c94a8b603146cd615b79e10ac204cd
Naveen Kumar <naveen330@example.com>
2026-03-18 18:02:12 +0530

    Array2

---
+++ Array2.java
@@ -0,0 +1,11 @@
+public class Array2 {
+    public static void main(String arg[]) {
+        System.out.println("Print given array ");
+        int A[] = { 1, 3, 4, 4, 5 };
+        int b[] = new int[5];
+        for (int i = 0; i < A.length; i++) {
+            System.out.println(A[i]);
+        }
+    }
+
+}
