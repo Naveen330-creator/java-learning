64c64e9eaf0922b8a42e54723a97c344115398de
Naveen Kumar <naveen330@example.com>
2026-03-18 19:34:20 +0530

    FindMaximum

---
+++ FindMaximum.java
@@ -0,0 +1,17 @@
+public class FindMaximum {
+    public static void main(String args[]) {
+        System.out.println("Maximum Element is ");
+        int A[] = { 3, 9, 7, 8, 12, 6, 15, 5, 4, 10 };
+        int max = A[0];
+        for (int i = 1; i < A.length; i++) {
+            if (max < A[i]) {
+                max = A[i];
+
+            }
+            // System.out.println(max);
+        }
+        System.out.println(max);
+
+    }
+
+}
