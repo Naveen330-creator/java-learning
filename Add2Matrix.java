9b7f196002b765d9ff18376f6fc2aeeb0d9d1d56
Naveen Kumar <naveen330@example.com>
2026-04-03 15:22:28 +0530

    Adding matrix

---
+++ Add2Matrix.java
@@ -0,0 +1,19 @@
+public class Add2Matrix {
+    public static void main(String args[]) {
+        int A[][] = { { 3, 9, 9 }, { 7, 6, 2 }, { 4, 3, 5 } };
+        int B[][] = { { 1, 5, 3 }, { 6, 8, 4 }, { 3, 9, 7 } };
+        int C[][] = new int[3][3];
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+                System.out.print(C[i][j] + " ");
+
+            }
+            System.out.println(" ");
+
+        }
+
+    }
+
+}
