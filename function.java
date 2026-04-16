b0ddb1fb6eafd97a44c2055ab7f410bc29aca666
Naveen Kumar <naveen330@example.com>
2026-04-16 22:43:14 +0530

    Function1

---
+++ function.java
@@ -0,0 +1,13 @@
+public class function {
+    public static void Demo() {
+        System.out.println("Hello Buddy ");
+        System.out.println("Open VS ");
+
+    }
+
+    public static void main(String args[]) {
+        Demo();
+
+    }
+
+}
