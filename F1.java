863293781b32e3d068fea4a72291e70f9d382913
Naveen Kumar <naveen330@example.com>
2026-05-26 14:21:00 +0530

    Function

---
+++ F1.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner;
+public class F1 {
+    public static void printHello() {
+        System.out.println("Hello ");
+
+    }
+
+    public static void main(String args[]) {
+        printHello();
+        // System.out.println("Hii");
+
+    }
+}
